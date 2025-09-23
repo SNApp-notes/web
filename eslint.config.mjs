382d1497224adb6972e8d38c20bca9310f604a2c
Jakub Jankiewicz <jcubic@onet.pl>
2025-09-24 00:27:22 +0200

    configure and run prettier again

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,14 +6,14 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: __dirname
 });
 
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
-    ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
-  },
+    ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts']
+  }
 ];
 
 export default eslintConfig;
