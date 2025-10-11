3806c823d2f93d69df827acf5fa80c7a16c23954
Jakub Jankiewicz <jcubic@onet.pl>
2025-10-12 00:04:51 +0200

    fix Better Auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,15 +1,15 @@
 import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
 import prisma from '@/lib/prisma';
-import { socialProviders } from 'better-auth/social-providers';
 
 export const auth = betterAuth({
-  providers: [
-    socialProviders.github({
+  secret: process.env.BETTER_AUTH_SECRET,
+  socialProviders: {
+    github: {
       clientId: process.env.GITHUB_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string
-    })
-  ],
+    }
+  },
   database: prismaAdapter(prisma, {
     provider: 'mysql'
   })
