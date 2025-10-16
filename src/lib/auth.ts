f73cb1b6a308b28941eb71937f2397098cc04cce
Jakub Jankiewicz <jcubic@onet.pl>
2025-10-17 00:27:15 +0200

    attempt to fix OAuth with GitHub

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,6 +5,7 @@ import prisma from '@/lib/prisma';
 import { sendEmail } from '@/lib/email';
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
   secret: process.env.BETTER_AUTH_SECRET,
   plugins: [nextCookies()],
   emailAndPassword: {
