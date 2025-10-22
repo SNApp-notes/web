6a16c3267adf80ec582c1368d2b000704d672668
Jakub Jankiewicz <jcubic@onet.pl>
2025-10-22 20:24:28 +0200

    add json reporter for coverage.el tool

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     css: true,
     coverage: {
       provider: 'v8',
-      reporter: ['text', 'lcov', 'html'],
+      reporter: ['text', 'lcov', 'html', 'json'],
       exclude: [
         'node_modules/',
         'src/test/',
