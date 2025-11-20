d7a56a039faf361c475f02e151eba19ce10657e5
Jakub Jankiewicz <jcubic@onet.pl>
2025-11-20 19:42:43 +0100

    fix prisma

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,11 +2,14 @@ import 'dotenv/config';
 import path from 'node:path';
 import { defineConfig, env } from 'prisma/config';
 export default defineConfig({
-  datasource: process.env.CI === 'true' ? {
-    url: 'file:./test.db'
-  } : {
-    url: env('DATABASE_URL'),
-    shadowDatabaseUrl: env('SHADOW_DATABASE_URL')
-  },
-  schema: path.join(process.env.CI ? 'prisma-e2e' : 'prisma-main', 'schema.prisma'),
+  datasource:
+    process.env.CI === 'true'
+      ? {
+          url: 'file:./test.db'
+        }
+      : {
+          url: env('DATABASE_URL'),
+          shadowDatabaseUrl: env('SHADOW_DATABASE_URL')
+        },
+  schema: path.join(process.env.CI ? 'prisma-e2e' : 'prisma-main', 'schema.prisma')
 });
