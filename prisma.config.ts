feac23708a417346ec6a62878285d4fd44ea67ef
Jakub Jankiewicz <jcubic@onet.pl>
2025-11-20 19:09:47 +0100

    migrate to prisma 7.0

---
+++ prisma.config.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+import path from 'node:path';
+import { defineConfig, env } from 'prisma/config';
+export default defineConfig({
+  datasource: process.env.CI === 'true' ? {
+    url: 'file:./test.db'
+  } : {
+    url: env('DATABASE_URL'),
+    shadowDatabaseUrl: env('SHADOW_DATABASE_URL')
+  },
+  schema: path.join(process.env.CI ? 'prisma-e2e' : 'prisma-main', 'schema.prisma'),
+});
